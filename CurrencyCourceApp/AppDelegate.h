bc60366ab4b75cd96c7609ef5255a6d243255b01
Elerman <stafedapple2015@ya.ru>
2017-02-28 21:48:42 +0300

    Initial Commit

---
+++ CurrencyCourceApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyCourceApp
+//
+//  Created by Elerman on 28.02.17.
+//  Copyright © 2017 Elerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrencyCourceApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CurrencyCourceApp
+//
+//  Created by Elerman on 28.02.17.
+//  Copyright © 2017 Elerman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
